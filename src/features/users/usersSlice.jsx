5a2e246c3747d133a5f15dfbe1cda5dc3f1b1c70
“Edu58” <edumuriithi58@gmail.com>
2022-08-21 16:21:27 +0300

    fix: all post authors are unknown

--- src/features/users/usersSlice.jsx
+++ src/features/users/usersSlice.jsx
@@ -1,15 +1,36 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-const initialState = [
-    { id: '0', name: 'john doe' },
-    { id: '1', name: 'bob junior' },
-]
+const BASE_URL = 'https://jsonplaceholder.typicode.com/users'
+
+// const initialState = [
+//     { id: '0', name: 'john doe' },
+//     { id: '1', name: 'bob junior' },
+// ]
+
+const initialState = []
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+    try {
+        const response = await fetch(BASE_URL, {
+            method: 'GET'
+        })
+        const data = response.json()
+        return data
+
+    } catch (error) {
+        return error.message
+    }
+})
 
 const usersSlice = createSlice({
     name: 'users',
     initialState,
-    reducers: {
-
+    reducers: {},
+    extraReducers(builder) {
+        builder
+            .addCase(fetchUsers.fulfilled, (state, action) => {
+                return action.payload
+            })
     }
 })
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import App from './App'
 import { store } from './app/store'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {fetchUsers} from './features/users/usersSlice'
+
+store.dispatch(fetchUsers())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.Fragment>
