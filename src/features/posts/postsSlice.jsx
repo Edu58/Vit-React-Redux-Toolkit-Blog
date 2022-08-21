086708af40cdf295c3746846ad24033d9e97afc7
“Edu58” <edumuriithi58@gmail.com>
2022-08-21 15:10:04 +0300

    rm postsSlice initialState

--- src/features/posts/postsSlice.jsx
+++ src/features/posts/postsSlice.jsx
@@ -1,34 +1,40 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 import { sub } from 'date-fns'
 
-const initialState = [
-    {
-        id: '1',
-        title: 'learning react + redux-toolkit',
-        content: 'I am making a simple blog using vite + react + react-bootstrap + redux-toolkit',
-        date: sub(new Date(), { minutes: 3 }).toISOString(),
-        reactions: {
-            thumbsUp: 0,
-            wow: 0,
-            heart: 0,
-            rocket: 0,
-            coffee: 0
-        }
-    },
-    {
-        id: '2',
-        title: 'react is simple',
-        content: 'reactjs is imple to learn and start using even as a beginner',
-        date: sub(new Date(), { minutes: 123 }).toISOString(),
-        reactions: {
-            thumbsUp: 0,
-            wow: 0,
-            heart: 0,
-            rocket: 0,
-            coffee: 0
-        }
-    },
-]
+const initialState = {
+    posts: [],
+    status: 'idle',
+    error: null
+}
+
+// const initialState = [
+//     {
+//         id: '1',
+//         title: 'learning react + redux-toolkit',
+//         content: 'I am making a simple blog using vite + react + react-bootstrap + redux-toolkit',
+//         date: sub(new Date(), { minutes: 3 }).toISOString(),
+//         reactions: {
+//             thumbsUp: 0,
+//             wow: 0,
+//             heart: 0,
+//             rocket: 0,
+//             coffee: 0
+//         }
+//     },
+//     {
+//         id: '2',
+//         title: 'react is simple',
+//         content: 'reactjs is imple to learn and start using even as a beginner',
+//         date: sub(new Date(), { minutes: 123 }).toISOString(),
+//         reactions: {
+//             thumbsUp: 0,
+//             wow: 0,
+//             heart: 0,
+//             rocket: 0,
+//             coffee: 0
+//         }
+//     },
+// ]
 
 const postSlice = createSlice({
     name: 'posts',
@@ -36,7 +42,7 @@ const postSlice = createSlice({
     reducers: {
         postAdded: {
             reducer(state, action) {
-                state.push(action.payload)
+                state.posts.push(action.payload)
             },
             prepare(title, content, userId) {
                 return {
@@ -60,7 +66,7 @@ const postSlice = createSlice({
 
         addReaction(state, action) {
             const { postId, reaction } = action.payload
-            const existing = state.find(post => post.id === postId)
+            const existing = state.posts.find(post => post.id === postId)
 
             if (existing) {
                 existing.reactions[reaction]++
@@ -69,7 +75,7 @@ const postSlice = createSlice({
     }
 })
 
-export const selectAllPosts = (state) => state.posts
+export const selectAllPosts = (state) => state.posts.posts
 
 export const { postAdded, addReaction } = postSlice.actions
 
